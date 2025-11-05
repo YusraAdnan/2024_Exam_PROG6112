df168c95121413a99ac78841ae92a82b3838bc7a
YusraAdnan <101713869+YusraAdnan@users.noreply.github.com>
2025-11-05 13:20:12 +0200

    updates

--- 2024_Exam_Q2/src/pkg2024_exam_q2/IMovieTickets.java
+++ 2024_Exam_Q2/src/pkg2024_exam_q2/IMovieTickets.java
@@ -11,6 +11,9 @@ public interface IMovieTickets
     /* Option 1: */
     boolean ValidateData(MovieTicketData movieTicketData);  
     
-    //Option 2: If you do not create a class to hold the MovieTicket Data separately you can make an alternate method like below
+    /*Option 2: If you do not create a class to hold the MovieTicket Data separately you can make an alternate method like below
+    (you will lose a few marks if you do this in you exam as this is not exactly implementing the interface as asked in the question paper
+    This is just another way of how you can do the above method without a class) */
+    
     boolean ValidateData2(String movieName, int numbOfTickets, double ticketPrice);  
 }
